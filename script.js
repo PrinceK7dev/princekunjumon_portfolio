d19b6bd2cb64530dc430389bd22f862b309e3256
PrinceK7dev <popkun7113@gmail.com>
2025-04-27 21:20:18 -0400

    Updated to new theme && Added new Projects.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize AOS Library
+  AOS.init({
+    duration: 1200,
+    once: true,
+  });
+
+
+  const buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+      button.style.transform = 'scale(1.1)';
+    });
+
+    button.addEventListener('mouseout', () => {
+      button.style.transform = 'scale(1)';
     });
   });
-  
+});
